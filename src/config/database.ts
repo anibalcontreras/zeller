d67755e46a72f9d509084fb3ad90628c2815a861
ANIBAL CONTRERAS <anibal.contreras@uc.cl>
2024-11-08 08:57:07 -0300

    Fix - Render database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,14 +5,22 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL || undefined,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT
-    ? Number(process.env.DATABASE_PORT)
-    : undefined,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  synchronize: true, // Cambiar a false en producción y usar migraciones
-  entities: [__dirname + "/../models/*.ts"],
+  ...(process.env.NODE_ENV === "production" && process.env.DATABASE_URL
+    ? { url: process.env.DATABASE_URL } // In production, use DATABASE_URL
+    : {
+        host: process.env.DATABASE_HOST, // In development, use these values
+        port: process.env.DATABASE_PORT
+          ? Number(process.env.DATABASE_PORT)
+          : undefined,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+      }),
+  synchronize: process.env.NODE_ENV !== "production", // False in production
+  logging: process.env.NODE_ENV !== "production",
+  entities: [
+    process.env.NODE_ENV === "production"
+      ? __dirname + "/../models/*.js" // In production, use JS files
+      : __dirname + "/../models/*.ts", // In development, use TS files
+  ],
 });
